5f3fac44afd06e09f5513919620c23b7404bb7c0
Srujan rai <raisrujan@gmail.com>
2024-07-21 09:40:48 +0530

    Update sending_code_with_gps_AP_captive_portal.ino

--- ESP32/sending_code_with_gps_AP_captive_portal.ino
+++ ESP32/sending_code_with_gps_AP_captive_portal.ino
@@ -43,7 +43,7 @@ void setup() {
       xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
           alert(xhr.responseText);
-          document.getElementById("message").value = ''; // Clear the input field after sending
+          document.getElementById("message").value = ''; 
         }
       };
       xhr.send();
